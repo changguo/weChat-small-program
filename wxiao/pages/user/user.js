2609b57f638c824c9baebad75c59482a6a1f1315
changguo <1149260569@qq.com>
2016-10-19 22:56:38 +0800

    second push

--- wxiao/pages/user/user.js
+++ wxiao/pages/user/user.js
@@ -1,27 +1,92 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    modalHidden: true,
+    toastHidden: true,
+
+    userInfo: {
+      username: '',
+      password: ''
+    },
+
+    actionSheet: {
+      actionSheetHidden: false,
+      actionSheetItems: ['催办', '举报']
+    }
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  onLoad: function(options) {
+    // console.log('loaded');
+  },
+  onReady: function() {
+    // console.log('ready');
+  },
+  onHide: function() {
+    // console.log('hidden');
+  },
+  onUnload: function() {
+    // console.log('unload');
+  },
+  onShow: function() {
+    var u = wx.getStorageSync('username');
+    if(!u) {
+      this.setData({
+        userInfo: {
+          username: '',
+          password: ''
+        },
+        modalHidden: false
+      });
+    }
+  },
+
+  actionConfirm: function () {
+    wx.setStorageSync('username', this.data.userInfo.username);
+    wx.setStorageSync('password', this.data.userInfo.password);
+    this.setData({
+      modalHidden: true
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-  	//调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-      that.update()
+  actionCancel: function () {
+    this.setData({
+      modalHidden: true
     })
+  },
+
+  saveUsername: function (event) {
+    this.setData({
+      'userInfo.username': event.detail.value
+    })
+  },
+  savePassword: function (event) {
+    this.setData({
+      'userInfo.password': event.detail.value
+    })
+  },
+
+  bindItemTap: function (event) {
+    switch (event.target.dataset.name) {
+      case '催办':
+        this.setData({
+          toastHidden: false
+        })
+        break;
+      case '举报':
+        console.log(1);
+        break;
+    }
+  },
+  cancelActionSheet: function () {
+    this.setData({
+      'actionSheet.actionSheetHidden': true
+    });
+  },
+  actionSheetChange: function () {
+    console.log(3);
+  },
+
+  toastChange: function () {
+    this.setData({
+      toastHidden: true
+    });
   }
-})
+});
